baa489ec11859f478b18409b8fe850ba55f7cd3d
Bart Spaans <bart.spaans@gmail.com>
2017-12-16 07:27:06 +0000

    Make test.sh generic

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@
 
 set -euf -o pipefail
 
-ROOT_DIR_NAME=user-service
-DOCKER_ROOT_REPO_PATH="/go/src/github.com/ankyra/"
-DOCKER_REPO_PATH="${DOCKER_ROOT_REPO_PATH}${ROOT_DIR_NAME}"
+PACKAGE_NAME="${INPUT_package_name}"
+DOCKER_PACKAGE_PATH="/go/src/${PACKAGE_NAME}"
+DOCKER_PACKAGE_PARENT_PATH=$(dirname "${DOCKER_PACKAGE_PATH}")
 DOCKER_VERSION=1.9.0
 
 cleanup_docker() {
@@ -15,12 +15,11 @@ cleanup_docker() {
 
 prepare_volume() {
     echo -n "Preparing Docker data volume..."
-    docker create -v ${DOCKER_ROOT_REPO_PATH} --name src golang:${DOCKER_VERSION} /bin/true 1>/dev/null 2>&1
-    docker cp "$PWD" "src:${DOCKER_ROOT_REPO_PATH}/tmp" 1>/dev/null
-    docker_run "${DOCKER_ROOT_REPO_PATH}" "mv tmp ${ROOT_DIR_NAME}" 1>/dev/null
+    docker create -v ${DOCKER_PACKAGE_PARENT_PATH} --name src golang:${DOCKER_VERSION} mkdir /code 1>/dev/null 2>&1
+    docker cp "$PWD" "src:${DOCKER_PACKAGE_PARENT_PATH}/tmp" 1>/dev/null
+    docker_run "${DOCKER_PACKAGE_PARENT_PATH}" "mv tmp ${DOCKER_PACKAGE_PATH}" 1>/dev/null
     echo "OK"
 }
-
 
 docker_run() {
     local cwd=$1
@@ -33,7 +32,7 @@ docker_run() {
 main() {
     cleanup_docker
     prepare_volume
-    docker_run "${DOCKER_REPO_PATH}" "go test -cover -v \$(/usr/local/go/bin/go list ./... | grep -v -E 'vendor' )"
+    docker_run "${DOCKER_PACKAGE_PATH}" "go test -cover -v \$(/usr/local/go/bin/go list ./... | grep -v -E 'vendor' )"
 }
 
 trap cleanup_docker EXIT
